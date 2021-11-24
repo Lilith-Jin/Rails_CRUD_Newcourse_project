6e7dc2e775fa7b387bf489dcb81e66e8f1907105
root <root@LAPTOP-F86AFOMK.localdomain>
2021-11-24 10:28:49 +0800

    add courses list

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,9 +1,38 @@
 class CoursesController < ApplicationController
   def index
-    @Course = Course.all
+    @courses = Course.all
   end
 
   def new
     @course = Course.new
   end
+
+  def show
+  end
+
+  def create
+    @course = Course.new(clean_params)
+
+    if @course.save
+      redirect_to courses_path, notice: "course created"
+    else
+      render :new
+    end
+  end
+
+  def clean_params
+    params.require(:course).permit(:name, :price, :intro, :hour)
+    
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def delete
+  end
+
+
 end
