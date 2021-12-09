fbd3e9af85eb89b66cd1f79afb1c6d1138b01ff7
Lilith <li6253462@gmail.com>
2021-12-09 20:42:33 +0800

    try

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,4 +1,6 @@
 class CoursesController < ApplicationController
+  before_action :set_course, only: [:edit, :update, :destroy]
+
   def index
     @courses = Course.all
   end
@@ -13,7 +15,7 @@ class CoursesController < ApplicationController
 
   def create
     # @course = Course.new(clean_params)
-
+    # render html: params
     if Course.new(clean_params).save 
       redirect_to courses_path, notice: "course created"
     else
@@ -22,11 +24,11 @@ class CoursesController < ApplicationController
   end
 
   def edit
-    @course = Course.find_by(id: params[:id])
+    # @course = Course.find_by(id: params[:id])
   end
 
   def update
-    @course = Course.find_by(id: params[:id])
+    # @course = Course.find_by(id: params[:id])
 
     if @course.update(clean_params)
       # 對要存進資料庫前的course過濾
@@ -38,14 +40,18 @@ class CoursesController < ApplicationController
   end
 
   def destroy
-    @course = Course.find_by(id: params[:id])
+    # @course = Course.find_by(id: params[:id])
     @course.destroy
     redirect_to courses_path, notice: "course deleted"
   end
 
   private
+  #creat和edit都要用到
   def clean_params
     params.require(:course).permit(:name, :price, :intro, :hour)
   end
-  
+  #edit,uodate,destroy都要用到，寫成private方法，掛在before_action
+  def set_course
+    @course = Course.find_by(id: params[:id])
+  end
 end
